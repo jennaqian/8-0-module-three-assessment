a34e8d3420f543d7d6f61a4b589181ffb5a716ac
Jenna Qian <jiayin1996@gmail.com>
2021-12-16 11:08:21 -0500

    Home Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home.js"
 import Navbar from "./components/Navbar.js"
 import People from "./components/People.js"
 import Locations from "./components/Locations.js"
+import Movies from "./components/Movies.js"
 
 function App() {
   return (
@@ -12,7 +13,10 @@ function App() {
         <Navbar />
       </nav>
       <main>
-        <h1>Hello, world!</h1>
+        <Route exact path="/" component={Home} />
+        <Route path="/movies" component={Movies} />
+        <Route path="people" component={People} />
+        <Route path="locations" component={Locations} />
       </main>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ class Home extends React.Component {
     render() {
         return (
             <div>
-
+                <h1>Welcome to GhibliApp</h1>
             </div>
         )
     }
